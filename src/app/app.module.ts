5a78106d78ecc3bfc2fb76dd7a97ac09357011a9
Canu Fabien <fabien.canu@gmail.com>
2023-05-19 08:33:28 +0200

    Up

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FormTaskComponent } from './form-task/form-task.component';
+import { ListTasksComponent } from './list-tasks/list-tasks.component';
+import { TaskComponent } from './task/task.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FormTaskComponent,
+    ListTasksComponent,
+    TaskComponent
   ],
   imports: [
     BrowserModule,
