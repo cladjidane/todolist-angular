b24f5d0bb70d61b95ea5189741ee6fbd90bfd6b2
Canu Fabien <fabien.canu@gmail.com>
2023-05-21 17:15:50 +0200

    up

--- src/app/task/task.component.ts
+++ src/app/task/task.component.ts
@@ -3,8 +3,9 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-task',
   templateUrl: './task.component.html',
-  styleUrls: ['./task.component.css']
+  styleUrls: ['./task.component.css'],
 })
 export class TaskComponent {
-
+  status: string = 'pending';
+  taskname: string = '';
 }
