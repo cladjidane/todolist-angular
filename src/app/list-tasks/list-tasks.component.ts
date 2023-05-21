fec5390108ffc6a718c6cbaeaeb89543a21ccae4
Canu Fabien <fabien.canu@gmail.com>
2023-05-21 22:28:56 +0200

    Up

--- src/app/list-tasks/list-tasks.component.ts
+++ src/app/list-tasks/list-tasks.component.ts
@@ -3,8 +3,24 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-list-tasks',
   templateUrl: './list-tasks.component.html',
-  styleUrls: ['./list-tasks.component.css']
+  styleUrls: ['./list-tasks.component.css'],
 })
 export class ListTasksComponent {
-
+  tasks = [
+    {
+      id: 'task-1',
+      taskname: 'Saluer le boss',
+      status: 'pending',
+    },
+    {
+      id: 'task-2',
+      taskname: 'Passer le balai',
+      status: 'finish',
+    },
+    {
+      id: 'task-3',
+      taskname: 'Travailler un peu quand même !',
+      status: 'pending',
+    },
+  ];
 }
