b9f0a3dc2e3064ba419758f8a65175cfb764c894
Canu Fabien <fabien.canu@gmail.com>
2023-05-22 16:49:55 +0200

    up

--- src/app/form-task/form-task.component.ts
+++ src/app/form-task/form-task.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'app-form-task',
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./form-task.component.css']
 })
 export class FormTaskComponent {
+  @Output() add: EventEmitter<any> = new EventEmitter();
 
+  addTask() {
+    this.add.emit(this.task);
+  }
 }
