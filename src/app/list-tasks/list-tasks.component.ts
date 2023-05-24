241aeec326f2467b4479e8da04cbd2357c40d285
Canu Fabien <fabien.canu@gmail.com>
2023-05-24 09:47:09 +0200

    up

--- src/app/list-tasks/list-tasks.component.ts
+++ src/app/list-tasks/list-tasks.component.ts
@@ -8,9 +8,14 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
 export class ListTasksComponent {
   @Input() tasks: string[] | undefined;
   @Output() delete: EventEmitter<string> = new EventEmitter<string>();
+  @Output() update: EventEmitter<string> = new EventEmitter<string>();
 
   deleteTask(task: string): void {
     this.delete.emit(task);
+  }
+
+  updateTask(task: string): void {
+    this.update.emit(task);
   }
 }
 
--- src/app/task/task.component.ts
+++ src/app/task/task.component.ts
@@ -8,13 +8,14 @@ import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 export class TaskComponent implements OnInit {
   @Input() task: any;
   @Output() delete: EventEmitter<any> = new EventEmitter();
+  @Output() update: EventEmitter<any> = new EventEmitter();
 
   ngOnInit(): void {
     console.log(this.task); // Affiche les tâches reçues dans la console
   }
 
   updateStatus(): void {
-    this.task.status = this.task.status === 'pending' ? 'finish' : 'pending';
+    this.update.emit(this.task);
   }
 
   deleteTask(): void {
