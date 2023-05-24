41e5cf9c1ed04ccab9c542ec8c923504e271de4f
Canu Fabien <fabien.canu@gmail.com>
2023-05-24 12:26:19 +0200

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { TaskService } from './task.service';
 
 @Component({
@@ -6,7 +6,7 @@ import { TaskService } from './task.service';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit{
+export class AppComponent {
   title = 'todolist-angular';
   tasks: any = []
 
